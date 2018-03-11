ce996154c7b20f4c4abf3c272512022aef7bc5b1
jonieva <jorgeonieva@gmail.com>
2018-03-10 21:42:24 -0500

    First submission

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -38,6 +38,7 @@ class ModelSelector(object):
         try:
             hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
                                     random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+
             if self.verbose:
                 print("model created for {} with {} states".format(self.this_word, num_states))
             return hmm_model
@@ -75,9 +76,36 @@ class SelectorBIC(ModelSelector):
         :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        best_model = None
+        best_score = float("inf")
+        n_features = self.X.shape[1]
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        for n_components in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                model = self.base_model(n_components)
+                model.fit(self.X, self.lengths)
+                logL = model.score(self.X, self.lengths)
+                N = self.X.shape[0]   # Number of data points
+
+                # p = total number of parameters in the model:
+                #   n_components * (n_components - 1) --> transition probabilities between states (the last row can be calculated
+                #                             because the total probability must sum 1.0, that's the reason of the -1 term)
+                #   n_components - 1 --> initial probabilities
+                #   n_components * n_features * 2 --> means and variances for each feature
+                p = (n_components ** 2) + (n_components * n_features * 2) - 1
+
+                bic = -2. * logL + p * np.log(N)
+
+                if bic < best_score:
+                    # Keep the model with the lowest score
+                    best_model = model
+                    best_score = bic
+            except Exception as ex:
+                # Nothing to do. Just the model could not be trained with this number of components
+                # print("Exception ocurred for word {} and {} components: {}".format(self.this_word, n_components, ex))
+                pass
+
+        return best_model
 
 
 class SelectorDIC(ModelSelector):
@@ -93,8 +121,34 @@ class SelectorDIC(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        best_model = None
+        best_score = float("-inf")
+        M = len(self.hwords)     # Total number of categories (words)
+        for num_components in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                model = self.base_model(num_components)
+                model.fit(self.X, self.lengths)
+                # Likehood term
+                logP = model.score(self.X, self.lengths)
+                # Anti-likehood terms
+                sum_ = 0.0
+                for word in (word for word in self.hwords if word != self.this_word):
+                    X, lengths = self.hwords[word]
+                    sum_ += model.score(X, lengths)
+                sum_ /= (M - 1)
+
+                dic = logP - sum_
+
+                if dic > best_score:
+                    best_model = model
+                    best_score = dic
+            except Exception as ex:
+                # Nothing to do. Just the model could not be trained with this number of components
+                #print("Exception ocurred for word {} and {} components: {}".format(self.this_word, num_components, ex))
+                pass
+
+        return best_model
+
 
 
 class SelectorCV(ModelSelector):
@@ -105,5 +159,94 @@ class SelectorCV(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        # Number of sequences for the current word
+        num_sequences = len(self.lengths)
+
+        # Number of folds by default used by KFold (it could be parametrized in the class)
+        # By default we will use the same number splits than sequences for this word, but we will limit it
+        K = min(num_sequences, 20)
+
+        if K == 0:
+            # No data points
+            return None
+        best_global_score = float("-inf")
+        best_global_model = None
+
+        if K == 1:
+            # Cross validation cannot be applied. Just return a trained model with the single sequence available
+            for num_components in range(self.min_n_components, self.max_n_components + 1):
+                try:
+                    model = self.base_model(num_components)
+                    model.fit(self.X, self.lengths)
+                    score = model.score(self.X, self.lengths)
+                    if score > best_global_score:
+                        # Keep the model that worked best for this number of components
+                        best_global_model = model
+                        best_global_score = score
+                except Exception as ex:
+                    # Nothing to do. Just the model could not be trained with this number of components
+                    # print("Exception occurred for word {} and {} components: {}".format(self.this_word, num_components,
+                    #                                                                    ex))
+                    pass
+            return best_global_model
+
+        # Apply cross validation
+        kf = KFold(n_splits=K, random_state=self.random_state, shuffle=True)
+        # print("Total sequences for word {}: {}. Number of splits: {}".format(self.this_word, num_sequences, K))
+
+        for num_components in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                score = 0.0
+                nfolds = 0
+                best_model = None
+                best_score = float("-inf")
+                # print ("Testing {} components...".format(num_components))
+                for train_index, test_index in kf.split(self.sequences):
+                    # Select indexes to make this training
+                    # print ("Word: {}".format(self.this_word))
+                    # print ("train_index: ", train_index)
+                    # print ("test_index: ", test_index)
+                    model = self.base_model(num_components)
+
+                    # Get the sequences based on the train index
+                    # Unfortunately we have to iterate because self.sequences is a list of lists with variable size
+                    seqs = []
+                    lengths = []
+                    for ix in train_index:
+                        for seq in self.sequences[ix]:
+                            seqs.append(np.array(seq))
+                        lengths.append(self.lengths[ix])
+                    x = np.array(seqs)
+                    # Train with train set
+                    model.fit(x, lengths)
+
+                    # Test with test set
+                    seqs = []
+                    lengths = []
+                    for ix in test_index:
+                        for seq in self.sequences[ix]:
+                            seqs.append(np.array(seq))
+                        lengths.append(self.lengths[ix])
+                    x = np.array(seqs)
+                    likehood = model.score(x, lengths)
+
+                    # Keep the best model for this number of components so far
+                    if likehood > best_score:
+                        best_model = model
+                        best_score = likehood
+
+                    # Add the score (the final result will be the average)
+                    score += likehood
+                    nfolds += 1
+                # The final components score will be the average score for all the folds
+                score /= nfolds
+                if score > best_global_score:
+                    # Keep the model that worked best for this number of components
+                    best_global_model = best_model
+                    best_global_score = score
+            except Exception as ex:
+                # Nothing to do. Just the model could not be trained with this number of components
+                #print("Exception occurred for word {} and {} components: {}".format(self.this_word, num_components, ex))
+                pass
+
+        return best_global_model
--- my_recognizer.py
+++ my_recognizer.py
@@ -22,4 +22,30 @@ def recognize(models: dict, test_set: SinglesData):
     guesses = []
     # TODO implement the recognizer
     # return probabilities, guesses
-    raise NotImplementedError
+    word_ix = 0
+
+    for word_ix, (X, length) in test_set.get_all_Xlengths().items():
+        # print ("IX: ", word_ix)
+        # print ("X: ", X)
+        # print ("length: ", length)
+        # Get the word to be guessed
+        word = test_set.wordlist[word_ix]
+        # Test each model and keep the best score
+        best_score = float("-INF")
+        guessed_word = None
+        probs = {}
+        for w, model in models.items():
+            try:
+                if model is not None:
+                    score = model.score(X, length)
+                    probs[w] = score
+                    if score > best_score:
+                        guessed_word = w
+                        best_score = score
+            except Exception as ex:
+                #print("Exception evaluating model for word {}: {}. Model: {}".format(w, ex, model))
+                pass
+        probabilities.append(probs)
+        guesses.append(guessed_word)
+
+    return probabilities, guesses
